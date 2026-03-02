89cf05fe3d4b7aa400d463ad22d5784cfbd0bcb0
Kartik Garg <kartikgarg0124@gmail.com>
2026-03-02 14:49:58 +0530

    path chnages

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 export default defineConfig({
+  base: "/",
   plugins: [
     react(),
   ],
@@ -14,7 +15,7 @@ export default defineConfig({
   },
   root: path.resolve(import.meta.dirname, "client"),
   build: {
-    outDir: path.resolve(import.meta.dirname, "dist/public"),
+    outDir: path.resolve(import.meta.dirname, "dist"),
     emptyOutDir: true,
   },
   server: {
